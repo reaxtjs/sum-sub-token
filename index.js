e40480cf1bdc531e1045add45f5aa981f0a1b557
aministrato <67193067+reaxtjs@users.noreply.github.com>
2023-09-25 11:46:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ const FormData = require("form-data");
 const app = express();
 const PORT = 4000;
 
+var allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://client.opofinance.com",
+];
 app.use(
   cors({
-    origin: "*",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   })
 );
 app.listen(PORT, () => {
